8189f39b5abd3126aaacce1e55e5ee7bb212bf5e
Daniel Sato <dksato@gmail.com>
2016-03-02 11:13:08 -0600

    Added minification for production

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,38 @@
 'use strict';
 let path = require('path');
+let webpack = require('webpack');
+
 const config = {
-  //devtool: 'eval-source-map',
+  devtool: 'cheap-module-source-map',
   entry: __dirname+'/client/app.jsx',
   output: {
     path: __dirname,
     file: 'bundle.js'
   },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      },
+      mangle: true,
+      preserveComments: false,
+      screwIE8: true
+
+    })
+  ],
   module: {
     loaders: [
       {
         loader: 'babel',
         test: /\.jsx?$/,
         query: {
-          presets: ['react','es2015']
+          presets: ['react','es2015'],
+          compact : false
         }
       }
     ]
